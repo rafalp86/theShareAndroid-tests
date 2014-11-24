cbd78aa43c6895b82fcec8103af1f8770cbf4791
rafalp86 <rafalpilecki@wp.pl>
2014-11-24 21:15:29 +0100

    Crate test to "Compose text message" Page

--- src/Page/AddPostPage.java
+++ src/Page/AddPostPage.java
@@ -1,6 +1,9 @@
 package Page;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import Base.GetBy;
 import Base.UI;
@@ -13,6 +16,18 @@ public class AddPostPage extends UI {
 	public WallPage  TapDone(){
 		  Click(DoneButton);
 		  return new WallPage();
+	}
+	public AddPostPage  TapFilter(String FilterText){
+		  Click(FilterByText(FilterText));
+		  return this;
+	}
+	public Integer GetCountCheckedFilter()
+	{
+		Integer checked= new Integer(0);
+		List<WebElement> allFilter= Finds(AllFillterCheckbox);
+		for(int i=0; i<allFilter.size();i++)
+			if(allFilter.get(i).isSelected()) checked++;
+		return checked;
 	}
 	//Set
 	public AddPostPage SetMessage(String messagetext)
@@ -35,13 +50,14 @@ public class AddPostPage extends UI {
 	}
 	public boolean FilterIsPresent(String filterText)
 	{
-		return ElementExist(GetBy.ClassAndText(Class.TextView, filterText));
+		return ElementExist(FilterByText(filterText));
 	}
 	//ELEMENTS
 	private By DoneButton= GetBy.ResourceId("com.example.theshare:id/button_done");	
 	private By ComposeTextArea= GetBy.ResourceId("com.example.theshare:id/compose_text");	
 	private By TitleElemet= GetBy.ClassAndText(Class.TextView, "Compose text message");
-	
+	private By FilterByText(String Text){return GetBy.ClassAndText(Class.TextView, Text);}
+	private By AllFillterCheckbox= GetBy.ResourceId("com.example.theshare:id/filter_checkbox");
 	
 	
 	//Choose  source Dialog
@@ -70,4 +86,5 @@ public class AddPostPage extends UI {
 	private By CameraSaurceElemet= GetBy.ClassAndText(Class.TextView, "Camera");
 	private By TextMessageElemet= GetBy.ClassAndText(Class.TextView, "Text message");
 	private By CancelButton= GetBy.ClassAndText(Class.TextView, "Cancel");
+	
 }
--- src/Tests/WhenUserDisplayedAddNewTextMessageView.java
+++ src/Tests/WhenUserDisplayedAddNewTextMessageView.java
@@ -21,12 +21,12 @@ public class WhenUserDisplayedAddNewTextMessageView extends ShareBase{
 		ConnectionWithApplication("WhenUserTryAddNewTextMessage");
 		AddMesageView =GoTo.ComposeTextView();
 	 }
-	@Test
+	//@Test
 	public void PageShouldHaveCorrectTitle()
 	{ //1.	Sprawdziæ czy ekran ma tytul „Compose text message..”
 		Assert.assertTrue(AddMesageView.PageTitleIsPresent(), "Can't see see [Compose text message] ");
 	}
-	@Test
+	//@Test
 	public void PageShouldHaveTextControlAndDoneButton()
 	{ //2.	Sprawdzic czy Elemety Done, pole tekstowe sa dostepne 
 		
@@ -34,10 +34,17 @@ public class WhenUserDisplayedAddNewTextMessageView extends ShareBase{
 		Assert.assertTrue(AddMesageView.MessageIsPresent(), "Can't see see [Message content] ");
 		
 	}
-	@Test(dataProvider = "Filter")
-	public void PageShouldHaveFewParameters(String Filter,Boolean notUsedparameter)
+//	@Test(dataProvider = "Filter")
+	public void PageShouldHaveFewParameters(String Filter,Integer notUsedparameter)
 	{ //3.	Sprawdziæ czy parametry "showe to" s¹ dostêpne (data Provider)
 		Assert.assertTrue(AddMesageView.FilterIsPresent(Filter), "Can't see see [Filter] ");		
+	}
+	
+	@Test(dataProvider = "Filter")
+	public void CheckboxInFilterShouldBeLinked(String Filter,Integer HowManyElementIsSelect)
+	{ //4.	Sprawdziæ zale¿noœci miedzy checkboxami
+		AddMesageView.TapFilter(Filter);
+		Assert.assertEquals(AddMesageView.GetCountCheckedFilter(), HowManyElementIsSelect);	
 	}
 	
 	
@@ -45,12 +52,12 @@ public class WhenUserDisplayedAddNewTextMessageView extends ShareBase{
 	  @DataProvider(name = "Filter")
 	  public  Object[][] Filtertext() {
 	        return new Object[][]{
-	        		{"Entire Alumni Program",true},
-	        		{"Entire Family Program",true},
-	        		{"People you are following",true},
-	        		{"People following you",true},
-	        		{"Only with men",true},
-	        		{"Select specific followers",true}};
+	        		{"Entire Alumni Program",1},
+	        		{"Entire Family Program",2},
+	        		{"People you are following",1},
+	        		{"People following you",2},
+	        		{"Only with men",3},
+	        		{"Select specific followers",2}};
 	    }
     
 	@AfterMethod
