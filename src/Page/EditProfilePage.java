bfedea5f5ded297cfec5c7bbe5c071e9abe91905
rafalp86 <rafalpilecki@wp.pl>
2014-12-04 20:14:18 +0100

    build fix

--- src/Page/EditProfilePage.java
+++ src/Page/EditProfilePage.java
@@ -2,16 +2,50 @@ package Page;
 
 import org.openqa.selenium.By;
 
+import Action.Gestures;
 import Base.GetBy;
 import Base.UI;
 
 public class EditProfilePage extends UI{
-
+	//TAP
+	public YourProfilePage TabSave()
+	{
+		Gestures.ScollTo(SaveElement);
+		Click(SaveElement);
+		return new YourProfilePage();
+	}
+	// SET TEXT
+	public EditProfilePage NewProfileName(String newProfileName)
+	{
+		SetText(EditProfileNameElement, newProfileName);
+		return this;
+	}
+	public EditProfilePage NewAboutYou(String newAboutYou)
+	{
+		SetText(AboutYouProfile, newAboutYou);
+		return this;
+	}
+	public EditProfilePage NewLocation(String newLocation)
+	{
+		SetText(EditLocationElement, newLocation);
+		return this;
+	}
+	// GET text
+	public String GetEmial()
+	{
+		return GetText(EditEmailElement);
+	}
+	// IS PRESENT
+	public boolean PageTitleIsPresent(){
+			return ElementExist(TitleElement);
+	}
 	
-	
-		
-	private By action_notificationsElement = GetBy.ResourceId("com.example.theshare:id/action_notifications");
-	private By notifications_iconElement = GetBy.ResourceId("com.example.theshare:id/notifications_icon");
-	private By containerElement = GetBy.ResourceId("com.example.theshare:id/container");
+	private By TitleElement = GetBy.ResourceId("android:id/action_bar_title");
+	private By EditImageElement = GetBy.ResourceId("com.example.theshare:id/edit_profile_change_image");
+	private By EditProfileNameElement = GetBy.ResourceId("com.example.theshare:id/edit_profile_username");
+	private By AboutYouProfile = GetBy.ResourceId("com.example.theshare:id/edit_profile_about_you");
+	private By EditLocationElement = GetBy.ResourceId("com.example.theshare:id/edit_profile_location");
+	private By EditEmailElement = GetBy.ResourceId("com.example.theshare:id/edit_profile_email");
+	private By SaveElement = GetBy.ResourceId("com.example.theshare:id/btn_save");
 
 }
