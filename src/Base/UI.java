e6f6e7debeec5b6b30e56be5c467d2af8f40e3f4
rafalp86 <rafalpilecki@wp.pl>
2015-04-15 21:49:23 +0200

    test fix

--- src/Base/UI.java
+++ src/Base/UI.java
@@ -151,14 +151,16 @@ public class UI extends ShareBase {
 	 public static void SetText(By element, String message,Boolean ... ClearContent) {
 		    System.out.println("Set tekst in "+ element+" ="+message);
 			WebElement textBox= Find(element);
-			if(ClearContent.length<0)  textBox.clear();
+			//if(ClearContent.length<0) 
+			textBox.clear();
 			textBox.sendKeys(message);
 			Gestures.HideKeyboard();
 		}
 	 
 		public static void SetText(WebElement element, String message,Boolean ... ClearContent) {
 		    System.out.println("Set tekst in "+ element+" ="+message);
-			if(ClearContent.length<0)element.clear();
+			//if(ClearContent.length<0)
+				element.clear();
 			element.sendKeys(message);
 			Gestures.HideKeyboard();
 		}
