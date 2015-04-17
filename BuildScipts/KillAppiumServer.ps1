b471082e5c9603b7a2066bc2b71eff435fbe149f
rafalp86 <rafalpilecki@wp.pl>
2015-04-17 19:27:31 +0200

    change kill process script

--- BuildScipts/KillAppiumServer.ps1
+++ BuildScipts/KillAppiumServer.ps1
@@ -1,13 +1,13 @@
 $appiumProcess='node'
-$emulatorProcess='emulator'
+$emulatorProcess="emulato*"
 $AppiumProcessCount=(Get-Process | Where {$_.ProcessName -eq $appiumProcess} |measure).Count
 $EmulatorProcessCount=(Get-Process | Where {$_.ProcessName -eq $emulatorProcess} |measure).Count
 
 Write-Host $AppiumProcessCount , $EmulatorProcessCount
  "Stop Appium server"
- Get-Process | Where {$_.ProcessName -eq $appiumProcess} | foreach{$_.Kill()}
+ Get-Process | Where {$_.ProcessName -like $appiumProcess} | foreach{$_.Kill()}
 "Kill emulator"
-Get-Process | Where {$_.ProcessName -eq $emulatorProcess} | foreach{$_.Kill()}
+Get-Process | Where {$_.ProcessName -like $emulatorProcess} | foreach{$_.Kill()}
    
 
 
