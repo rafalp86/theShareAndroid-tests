a6cfe5a874d8ed7e610574488dae849d578bbb61
rafalp86 <rafalpilecki@wp.pl>
2015-05-20 20:56:50 +0200

    create Screenshot
    create Screenshot when test faild

--- src/Base/ShareBase.java
+++ src/Base/ShareBase.java
@@ -3,10 +3,18 @@ package Base;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
+import javax.imageio.ImageIO;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -15,6 +23,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+
 
 import Page.LogInPage;
 
@@ -52,6 +61,7 @@ public class ShareBase   {
 		}
 		catch (Exception ex)
 		{
+			ScreenshotDesktop("FAIL"+TestName);
 			Assert.fail("Problem with create Webdrive instantion: "+ ex.toString());
 		}
 		 driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
@@ -83,7 +93,23 @@ public class ShareBase   {
           
         }
 	 }
-	
+	protected void ScreenshotDesktop(String fileName)
+	{
+		Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		BufferedImage capture;
+		try {
+			File screenshotDir = new File("Screenshot");
+        	if (!screenshotDir.exists()) screenshotDir.mkdir();
+			capture = new Robot().createScreenCapture(screenRect);
+			ImageIO.write(capture, "jpg", new File("Screenshot/"+fileName + "_TheShare.png"));
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	@AfterMethod
 	protected  void ScreenshotWhenFail(ITestResult result) {
 		  if (result.getStatus() == ITestResult.FAILURE) {
